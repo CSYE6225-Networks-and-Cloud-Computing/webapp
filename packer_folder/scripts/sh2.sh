12f98ee5b66d882579d8b0256c9a00b367c934ae
Hir Desai <144550646+desaihir@users.noreply.github.com>
2024-10-17 14:36:23 -0400

    Update sh2.sh

--- packer_folder/scripts/sh2.sh
+++ packer_folder/scripts/sh2.sh
@@ -32,16 +32,16 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 # Create the database and user
-# DB_NAME="dbcreatetrial"
-# DB_USER="postgres"
-# DB_PASSWORD="postgres"
+DB_NAME_1="user"
+DB_USER_1="postgres"
+DB_PASSWORD_1="postgres"
 
 # Create the database
-sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME};"
-echo "HIRRRR CHECK WHAT IS THE DATABASE NAME?: ${DB_NAME}"
+sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME_1};"
+echo "HIRRRR CHECK WHAT IS THE DATABASE NAME?: ${DB_NAME_1}"
 # Create the user (if needed)
-echo "HIRRRR CHECK WHAT IS THE PASSWORD: ${DB_PASSWORD}"
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD ${DB_PASSWORD};"
+echo "HIRRRR CHECK WHAT IS THE PASSWORD: ${DB_PASSWORD_1}"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD ${DB_PASSWORD_1};"
 
 # Grant all privileges on the database to the user
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME_1} TO ${DB_USER_1};"
