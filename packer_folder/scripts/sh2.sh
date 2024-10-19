d5a1f4f5a5eac666609169dcfa8843124f331cab
Hir Desai <hirdesai@Hirs-MacBook-Pro.local>
2024-10-19 15:32:20 -0400

    updating packerBuild CI

--- packer_folder/scripts/sh2.sh
+++ packer_folder/scripts/sh2.sh
@@ -24,7 +24,7 @@
 
 # Install PostgreSQL
 echo "-------Installing PostgreSQL-------"
-sudo apt-get install -y postgresql postgresql-contrib
+# sudo apt-get install -y postgresql postgresql-contrib
 
 # Start PostgreSQL service
 echo "-------Starting PostgreSQL service-------"
@@ -32,16 +32,16 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 # Create the database and user
-# DB_NAME="dbcreatetrial"
-# DB_USER="postgres"
-# DB_PASSWORD="postgres"
+DB_NAME_1="user"
+DB_USER_1="postgres"
+DB_PASSWORD_1="postgres"
 
 # Create the database
-sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME};"
-echo "HIRRRR CHECK WHAT IS THE DATABASE NAME?: ${DB_NAME}"
+sudo -u postgres psql -c "CREATE DATABASE 'user';"
+# echo "HIRRRR CHECK WHAT IS THE DATABASE NAME?: ${DB_NAME_1}"
 # Create the user (if needed)
-echo "HIRRRR CHECK WHAT IS THE PASSWORD: ${DB_PASSWORD}"
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD ${DB_PASSWORD};"
+# echo "HIRRRR CHECK WHAT IS THE PASSWORD: ${DB_PASSWORD_1}"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
 # Grant all privileges on the database to the user
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE user TO postgres;"
