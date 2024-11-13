3b271dfd16892d9acf7f04346c9f52615cbc69e5
Hir Desai <hirdesai@Hirs-MacBook-Pro.local>
2024-11-12 19:20:49 -0500

    updated functions for A08

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const { signup, loadDetails, basicAuth, updateUser, uploadProfilePicture, deleteProfilePicture, getProfilePicture } = require('../controller/authController');
+const { signup, loadDetails, basicAuth, verifyEmail, checkVerified, updateUser, uploadProfilePicture, deleteProfilePicture, getProfilePicture } = require('../controller/authController');
 const setHeaders = require('../middleware/setHeaders');
 
 const router = express.Router();
@@ -9,7 +9,10 @@ router.use(setHeaders);
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.route('/').post(signup);
+router.route('/self/verify')
+  .get(verifyEmail);
 
+  
 // unsupported methods on /self
 router.route('/self')
   .delete((req, res) => res.status(405).send())  // DELETE not allowed
@@ -18,8 +21,8 @@ router.route('/self')
   .options((req, res) => res.status(405).send()); // OPTIONS not allowed
 
 router.route('/self')
-  .get(basicAuth, loadDetails)
-  .put(basicAuth, updateUser);
+  .get(basicAuth, checkVerified, loadDetails)
+  .put(basicAuth, checkVerified, updateUser)
 
 router.route('/self/pic')
   // .delete((req, res) => res.status(405).send())  // DELETE not allowed
@@ -30,30 +33,10 @@ router.route('/self/pic')
 
 
 router.route('/self/pic')
-  .get(basicAuth, getProfilePicture)
-  .post(basicAuth, upload.single('image'), uploadProfilePicture)
-  .delete(basicAuth, deleteProfilePicture);
+  .get(basicAuth, checkVerified, getProfilePicture)
+  .post(basicAuth, checkVerified, upload.single('image'), uploadProfilePicture)
+  .delete(basicAuth, checkVerified, deleteProfilePicture)
+
+// router.get('/verify', verifyEmail);
 
 module.exports = router;
-
-// const express = require('express');
-// const { signup, loadDetails, basicAuth, updateUser } = require('../controller/authController');
-// const setHeaders = require('../middleware/setHeaders');
-
-// const router = express.Router();
-// router.use(setHeaders);
-
-// router.route('/').post(signup); // create user
-
-// // unsupported methods on /self
-// router.route('/self')
-//   .delete((req, res) => res.status(405).send())  // DELETE not allowed
-//   .patch((req, res) => res.status(405).send())   // PATCH not allowed
-//   .head((req, res) => res.status(405).send())    // HEAD not allowed
-//   .options((req, res) => res.status(405).send()); // OPTIONS not allowed
-
-// router.route('/self')
-// .get(basicAuth, loadDetails)  // view user details
-// .put(basicAuth, updateUser);  // update user
-
-// module.exports = router;
